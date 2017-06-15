a5dfc58f12c7ee78b6e4f00a2e4ec1ce33ac463e
rogchev <rogchev@gmail.com>
2017-06-15 21:54:24 +0300

    Added Models library
    Added Player, Coin, Position class in the library

---
+++ StickyHands/Models/Coin.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class Coin
+    {
+        public Coin(char symbol, int value)
+        {
+            this.Symbol = symbol;
+            this.Value = value;
+        }
+        public char Symbol { get; set; }
+        public int Value { get; set; }
+    }
+}
---
+++ StickyHands/Models/Player.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Utilities;
+
+namespace Models
+{
+    public class Player
+    {
+        public Player(string name)
+        {
+            this.Name = name;
+            this.Position = new Position(Console.WindowWidth / 2, Console.WindowHeight / 2);
+            this.Points = 0;
+        }
+        public string Name { get; set; }
+        public int Points { get; set; }
+        public Position Position { get; set; }
+        public IDictionary<char, int> CoinsCollected { get; set; }
+    }
+}
